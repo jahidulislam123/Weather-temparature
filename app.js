048fe7bca92596af8d40e593df960f506fd1db8d
unknown <islam15-3879@diu.edu.bd>
2022-03-02 21:48:08 +0600

    added

--- app.js
+++
@@ -1,6 +0,0 @@
-const API_KEY =`d352b6889cfd62c4432fec277bf8f691`;
-const searchTemparature=()=>{
-    const city =document.getElementById('city-name').value;
-    const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
-    console.log('clicked');
-}
---
+++ temperature-hot-master/app.js
@@ -0,0 +1,22 @@
+const API_KEY =`d352b6889cfd62c4432fec277bf8f691`;
+const searchTemparature=()=>{
+    const city =document.getElementById('city-name').value;
+    const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    // console.log(url);
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>displayTemparature(data));
+}
+
+const setInnerText = (id,text) =>{
+    document.getElementById(id).innerText=text;
+}
+const displayTemparature =(temparature)=>{
+    
+    setInnerText('city',temparature.name);
+    setInnerText('temparature',temparature.main.temp);
+    setInnerText('condition',temparature.weather[0].main);
+    console.log(temparature);
+
+
+}

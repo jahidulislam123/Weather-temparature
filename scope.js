68e29c660d3733128fb193865b5e5c6ebf6e363c
unknown <islam15-3879@diu.edu.bd>
2022-03-02 23:04:32 +0600

    added

---
+++ scope.js
@@ -0,0 +1,19 @@
+function patricai (age,earning){
+    console.log(trueAge);
+    var decission=canMarry(15000);
+    console.log(decission);
+    console.log(age);
+    console.log(earning);
+    var trueAge =age+7;
+     function canMarry (expense){
+        const remaining =earning-expense;
+        console.log(showing);
+        if(remaining>10000){
+            return true;
+            var showing =500;
+            
+        }
+        return false;
+    }
+}
+patricai(21,50000);
